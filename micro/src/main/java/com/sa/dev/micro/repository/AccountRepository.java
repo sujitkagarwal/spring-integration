699e5566e75065452c1359977cad653f9037958f
sujitagarwal <sujit21agarwal@gmail.com>
2017-08-31 22:59:28 +0200

    test auto build 1

--- micro/src/main/java/com/sa/dev/micro/repository/AccountRepository.java
+++ micro/src/main/java/com/sa/dev/micro/repository/AccountRepository.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Integer> {
-
     Account findByAccountNumber(String accountNumber);
     /**
      * Fetch the number of accounts known to the system.
